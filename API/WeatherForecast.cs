8525dc81269fe1b92509ded36ae4259438344cbc
nathanielDuro <nathanielDuro@users.noreply.github.com>
2023-05-11 11:39:46 +0800

    End of section 2

---
+++ API/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace API;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    //? means -optional- in .net 6 introduces nullable property
+    public string Summary { get; set; }
+}
