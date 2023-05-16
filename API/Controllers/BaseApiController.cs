b7bb560a501115e7db4edcbb1d6d1955242d16f5
nathanielDuro <nathanielDuro@users.noreply.github.com>
2023-05-16 11:56:45 +0800

    End of section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,12 +1,16 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")] // localhost:5000/weatherforecast
-
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
         
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
     }
 }
